b72a267c0c90161fa83a2d8a186d0a1108dfe584
Josh Snyder <josh@code406.com>
2011-05-01 00:32:16 -0400

    Lots of changes: File reading code in read_whole_file.spp Runner code in open.spp Encapsulated the sha1 single block algorithms into a macro

--- initial.s
+++ initial.s
@@ -1,73 +1,4 @@
-.data
-h0: .word 0x67452301
-h1: .word 0xEFCDAB89
-h2: .word 0x98BADCFE
-h3: .word 0x10325476
-h4: .word 0xC3D2E1F0
-
-k0: .word 0x5A827999
-k20:.word 0x6ED9EBA1
-k40:.word 0x8F1BBCDC
-k60:.word 0xCA62C1D6
-
-.text
-
-@message = $a0    #pointer to message
-@hlength = $a1  #length of message (in bits)
-@llength = $a2
-
-    sll     $t0, @message, 30       #$t0 CONTAINS 0 if the message is word aligned
-    sltu    $v0, $0, $t0
-    bne     $v0, $0, end
-
-@blocks     = $t0
-@bits       = $t1
-
-    srl     $t0, @llength, 3        
-    sll     $t1, @hlength, 29
-    or      $t0, $t1, $t0
-    srl     $t0, $t0, 6
-    sll     $t0, $t0, 6             #$t0 CONTAINS the length of the message in bytes, truncated to the lowest block
-
-    sll     $t1, @llength, 23       
-    srl     @bits, @llength, 23     #$t1 CONTAINS (number of bits) % 512
-
-@srcitr     = $t3
-@dstitr     = $t4
-@dstend     = $t5
-@mask       = $t6
-@data       = $t7
-    
-    move    @srcitr, @message
-    move    @dstitr,                #assign the memory location of the last block to @dstitr
-    addiu   @dstend, @dstitr, 64    #there are 64 bytes in a 512 bit block
-
-copy_loop:
-    sltu    @mask, $0, @bits
-    sra     @mask, @mask, @bits
-    lw      @data, 0[@srcitr]
-    
-    sw      @data, 0[@dstitr]
-    addiu   @srcitr, 4
-    addiu   @dstitr, 4
-    ble     @dstitr, @dstend, copy_loop
-    
-addiu   $sp, $sp, -320
-@chunk      =
-
-@h0         =
-@h1         =
-@h2         =
-@h3         =
-@h4         =
-
-    lw @h0, 0[h0]
-    lw @h1, 0[h1]
-    lw @h2, 0[h2]
-    lw @h3, 0[h3]
-    lw @h4, 0[h4]
-    
-main_loop:                  #loop over message chunks
+#define do_sha_block
 
 #1. Prepare the message schedule
 
@@ -121,10 +52,10 @@ main_loop:                  #loop over message chunks
     lw @data, 60[@chunk]
     sw @data, 60[$sp]
 
-@temp = 
-@w =
-@i =
-@limit =
+@temp = $t5
+@w = $t6
+@i = $t7
+@limit = $s0
 
 #W_t(i) = ROTL_1(W[t−3] ⊕ W[t−8] ⊕ W[t−14] ⊕ W[t−16]) where 16 ≤ t ≤ 79
 
@@ -140,16 +71,21 @@ extend_loop:                #extend the message schedule
     lw      @temp, -64[@i]
     xor     @w, @w, @temp
     
-    srl     @temp, @w, 31
+    srl     @temp, @w, 31		#leftrotate @w by 1
     sll     @w, @w, 1
     or      @w, @w, @temp
     sw      @w, 0[@i]
     
     addiu   @i, @i, 4
-    sltu    @temp, @i, @limit
-    bne     @temp, $0, extend_loop
+	blt		@i, @limit, extend_loop
     
 # 2. Initialize the five working variables, a, b, c, d, and e, with the (i-1)st hash value:
+
+@a = $s1
+@b = $s2
+@c = $s3
+@d = $s4
+@e = $s5
 
     move    @a, @h0
     move    @b, @h1
@@ -182,7 +118,9 @@ confuse_loop:
     bltz    @temp, case40
     
     j       case60              # 60 ≤ i ≤ 79
-        
+
+@f = $s6
+     
 case0:
 #Ch(b, c, d) = (b ∧ c) ⊕ ( ¬ b ∧ d) 
 
@@ -192,7 +130,7 @@ case0:
     and     @temp, @temp, @d    #@temp CONTAINS ¬ b ∧ d
     xor     @f, @f, @temp       #@f    CONTAINS (b ∧ c) ⊕ (¬ b ∧ d)
     
-    lw      @temp, 0[k0]
+    li      @temp, 0x5A827999
     addu    @f, @f, @temp
     
     j       end_case
@@ -202,7 +140,7 @@ case20:
     xor     @f, @b, @c
     xor     @f, @f, @d
     
-    lw      @temp, 0[k20]
+    li      @temp, 0x6ED9EBA1
     addu    @f, @f, @temp
 
     j       end_case
@@ -215,8 +153,8 @@ case40:
     
     and     @temp, @c, @d
     xor     @f, @f, @temp
-    
-    lw      @temp, 0[k40]
+   
+    li      @temp, 0x8F1BBCDC
     addu    @f, @f, @temp
     
     j       end_case
@@ -226,12 +164,16 @@ case60:
     xor     @f, @b, @c
     xor     @f, @f, @d
     
-    lw      @temp, 0[k60]
+    li      @temp, 0xCA62C1D6 
     addu    @f, @f, @temp
+
 end_case:
 
     addu    @f, @f, @e              #@f CONTAINS f_t(b, c, d) + e + K_t
-        
+
+
+	#TODO: Do I really need to use $at?
+
     sll     @temp, @a, 5
     srl     $at, @a, 27
     or      @temp, @temp, $at       #@temp CONTAINS ROTL_5(a)
@@ -254,6 +196,7 @@ end_case:
     
     addiu   @i, @i, 4
     
+	#If i<80, we're not done yet.  Go back to the top of confuse_loop
     addiu   @temp, @i, -320         #80 words == 320 bytes
     bltz    @temp, confuse_loop
     
@@ -262,10 +205,65 @@ end_case:
     addu    @h2, @h2, @c
     addu    @h3, @h3, @d
     addu    @h4, @h4, @e
+
+#end
+
+@message = $a0      #pointer to message
+@hlength = $a1      #length of message in bits, high part
+@llength = $a2		#length of message in bits, low part
+
+    sll     $t0, @message, 30       #$t0 CONTAINS 0 if the message is word aligned
+    sltu    $v0, $0, $t0			#If not word aligned, set error code
+    bne     $v0, $0, end			#Give up if not word aligned
+
+@blocks     = $t0
+@bits       = $t1
+
+    srl     $t0, @llength, 3
+    sll     $t1, @hlength, 29
+    or      $t0, $t1, $t0
+    srl     $t0, $t0, 6
+    sll     $t0, $t0, 6             #$t0 CONTAINS the length of the message in bytes, truncated to the lowest block
+
+    sll     $t1, @llength, 23       
+    srl     @bits, @llength, 23     #$t1 CONTAINS (number of bits) % 512
+
+@srcitr     = $t3
+@dstitr     = $t4
+@dstend     = $t5
+@mask       = $t6
+@data       = $t7
     
+    move    @srcitr, @message
+    move    @dstitr,                #assign the memory location of the last block to @dstitr
+    addiu   @dstend, @dstitr, 64    #there are 64 bytes in a 512 bit block
+
+copy_loop:
+    sltu    @mask, $0, @bits
+    sra     @mask, @mask, @bits
+    lw      @data, 0[@srcitr]
     
+    sw      @data, 0[@dstitr]
+    addiu   @srcitr, 4
+    addiu   @dstitr, 4
+    ble     @dstitr, @dstend, copy_loop
     
+addiu   $sp, $sp, -320
+@chunk      =
+
+@h0         = $t0
+@h1         = $t1
+@h2         = $t2
+@h3         = $t3
+@h4         = $t4
+
+    li @h0, 0x67452301
+    li @h1, 0xEFCDAB89
+    li @h2, 0x98BADCFE
+    li @h3, 0x10325476
+    li @h4, 0xC3D2E1F0
     
+main_loop:                  #loop over message chunks
     
     
     
@@ -278,11 +276,7 @@ end_case:
     
     
 
-@a =
-@b =
-@c =
-@d =
-@e =
+
 @f =
 @temp =
 
